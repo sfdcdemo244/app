42016e1823d6b8eeebc4ce718e0360d292fd8f19
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-15 20:51:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,24 +13,20 @@
    }
 
  $sql =<<<EOF
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (1, 'Paul', 32, 'California', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+      SELECT * from COMPANY;
 EOF;
 
    $ret = pg_query($db, $sql);
    if(!$ret) {
       echo pg_last_error($db);
-   } else {
-      echo "Records created successfully\n";
+      exit;
+   } 
+   while($row = pg_fetch_row($ret)) {
+      echo "ID = ". $row[0] . "\n";
+      echo "NAME = ". $row[1] ."\n";
+      echo "ADDRESS = ". $row[2] ."\n";
+      echo "SALARY =  ".$row[4] ."\n\n";
    }
+   echo "Operation done successfully\n";
    pg_close($db);
 ?>
