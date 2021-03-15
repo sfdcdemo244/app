d3823d6aa11298f53c7996225f3873792189ca3c
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-15 20:50:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,20 +12,25 @@
       echo "Opened database successfully\n";
    }
 
-   $sql =<<<EOF
-      CREATE TABLE COMPANY
-      (ID INT PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      AGE            INT     NOT NULL,
-      ADDRESS        CHAR(50),
-      SALARY         REAL);
+ $sql =<<<EOF
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (1, 'Paul', 32, 'California', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
 EOF;
 
    $ret = pg_query($db, $sql);
    if(!$ret) {
       echo pg_last_error($db);
    } else {
-      echo "Table created successfully\n";
+      echo "Records created successfully\n";
    }
    pg_close($db);
 ?>
